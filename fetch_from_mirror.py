dad39a61d48bf61884241f38735322612bc912d1
Denick <1049485+apie@users.noreply.github.com>
2019-01-12 14:15:31 +0100

    Less scripty
    And return immediately if there is nothing to download.

--- fetch_from_mirror.py
+++ fetch_from_mirror.py
@@ -49,55 +49,63 @@ def download_ep(ep_url):
     db_logic.mark_ep_as_downloaded(ep_id)
     get_sub(local_file)
 
-ep_names = {}
-shows = db_logic.get_shows_from_db()
-for ep_id, info in db_logic.get_eps_from_db().items():
-  if info.get('downloaded'):
-    #print('Already downloaded: {}'.format(info['name']))
-    continue
-  ep_names[ep_id] = dict(
-    show_name=shows[info['show_id']]['name'],
-    variants=[
-      print_ep(season=info['season'], episode=info['number']),
-      print_ep(season=info['season'], episode=info['number'], v=2),
-    ]
-  )
+def do_fetch():
+  ep_names = {}
+  shows = db_logic.get_shows_from_db()
+  for ep_id, info in db_logic.get_eps_from_db().items():
+    if info.get('downloaded'):
+      #print('Already downloaded: {}'.format(info['name']))
+      continue
+    ep_names[ep_id] = dict(
+      show_name=shows[info['show_id']]['name'],
+      variants=[
+        print_ep(season=info['season'], episode=info['number']),
+        print_ep(season=info['season'], episode=info['number'], v=2),
+      ]
+    )
 
-mirror_pages = []
-for mirror_url in settings.MIRROR_URLS:
-  mirror_page_response = requests.get(mirror_url, auth=settings.AUTH)
-  mirror_page_response.raise_for_status()
-  mirror_page = html.fromstring(mirror_page_response.text)
-  mirror_pages.append((mirror_page_response.url, mirror_page))
-for ep_id, ep_name in ep_names.items():
-  # Need to match case insensitive. Use a workaround with translate() for Xpath 1.0
-  current_url = None
-  for url, mirror_page in mirror_pages:
-    current_url = url
-    for ep_name_variant in ep_name['variants']:
-      search_txt = '{show} {ep_name}'.format(show=ep_name['show_name'], ep_name=ep_name_variant).replace(' ','.').replace("'", '').lower()
-      ep_folder_hrefs = mirror_page.xpath("//a[contains(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz'), '{}')]".format(search_txt))
+  if not ep_names:
+    # No new eps
+    return
+
+  mirror_pages = []
+  for mirror_url in settings.MIRROR_URLS:
+    mirror_page_response = requests.get(mirror_url, auth=settings.AUTH)
+    mirror_page_response.raise_for_status()
+    mirror_page = html.fromstring(mirror_page_response.text)
+    mirror_pages.append((mirror_page_response.url, mirror_page))
+  for ep_id, ep_name in ep_names.items():
+    # Need to match case insensitive. Use a workaround with translate() for Xpath 1.0
+    current_url = None
+    for url, mirror_page in mirror_pages:
+      current_url = url
+      for ep_name_variant in ep_name['variants']:
+        search_txt = '{show} {ep_name}'.format(show=ep_name['show_name'], ep_name=ep_name_variant).replace(' ','.').replace("'", '').lower()
+        ep_folder_hrefs = mirror_page.xpath("//a[contains(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz'), '{}')]".format(search_txt))
+        if ep_folder_hrefs:
+          break
       if ep_folder_hrefs:
         break
-    if ep_folder_hrefs:
-      break
-  if not ep_folder_hrefs:
-    continue
-  # If multiple matches are there for 1 ep, take the first match
-  ep_folder_href = ep_folder_hrefs[0]
-  ep_folder_url = urljoin(current_url, ep_folder_href.attrib['href'])
-  if is_downloadable(ep_folder_url, settings.AUTH):
-      # File. Download it
-      download_ep(ep_folder_url)
+    if not ep_folder_hrefs:
       continue
-  # Otherwise it is a folder. Open it.
-  ep_page_response = requests.get(ep_folder_url, auth=settings.AUTH)
-  ep_page_response.raise_for_status()
-  ep_page = html.fromstring(ep_page_response.text)
-  ep_hrefs = ep_page.xpath("//a[text() != '' and text() !='Name' and text() != 'Last modified' and text() !='Parent Directory' and text() != 'Show page in text format']")
-  for ep_href in ep_hrefs:
-    ep_filename = ep_href.attrib['href']
-    ep_url = urljoin(ep_page_response.url, ep_filename)
-    if is_downloadable(ep_url):
-      download_ep(ep_url)
+    # If multiple matches are there for 1 ep, take the first match
+    ep_folder_href = ep_folder_hrefs[0]
+    ep_folder_url = urljoin(current_url, ep_folder_href.attrib['href'])
+    if is_downloadable(ep_folder_url, settings.AUTH):
+        # File. Download it
+        download_ep(ep_folder_url)
+        continue
+    # Otherwise it is a folder. Open it.
+    ep_page_response = requests.get(ep_folder_url, auth=settings.AUTH)
+    ep_page_response.raise_for_status()
+    ep_page = html.fromstring(ep_page_response.text)
+    ep_hrefs = ep_page.xpath("//a[text() != '' and text() !='Name' and text() != 'Last modified' and text() !='Parent Directory' and text() != 'Show page in text format']")
+    for ep_href in ep_hrefs:
+      ep_filename = ep_href.attrib['href']
+      ep_url = urljoin(ep_page_response.url, ep_filename)
+      if is_downloadable(ep_url):
+        download_ep(ep_url)
+
+if __name__ == '__main__':
+  do_fetch()
 
